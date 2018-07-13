fbb82f9fc75ec56d7495d007871271abfc508a2f
Riley Schnee <rileyschnee@gmail.com>
2018-07-13 11:40:18 -0700

    Initial Commit

---
+++ DayTripper/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DayTripper
+//
+//  Created by Riley Schnee on 7/13/18.
+//  Copyright © 2018 MakerApps. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
